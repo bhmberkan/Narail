c1f044a066c8207b39717e39621c34bf350d9d16
bhmberkan <berkanburakturgut@gmail.com>
2024-02-18 16:11:16 +0300

    Admin panelinde yazar ekeleme kısmını oluşturdum.

---
+++ Narail/Controllers/AuthorAdminController.cs
@@ -0,0 +1,19 @@
+﻿using Narail.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Narail.Controllers
+{
+    public class AuthorAdminController : Controller
+    {
+        // GET: AuthorAdmin
+        NarailDBEntities db = new NarailDBEntities();
+        public ActionResult Index()
+        {
+            return View(db.Author.ToList());
+        }
+    }
+}
